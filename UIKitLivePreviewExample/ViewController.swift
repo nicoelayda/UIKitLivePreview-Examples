241aa87064ba8e84dbe60d39fbd9cf4b476b00a6
Dominic Elayda <nico@elayda.com>
2021-04-28 22:21:34 +0800

    Create sample ViewController preview.

--- UIKitLivePreviewExample/ViewController.swift
+++ UIKitLivePreviewExample/ViewController.swift
@@ -7,12 +7,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var label: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
         view.backgroundColor = .white
+        view.addSubview(label)
+        
+        setupConstraints()
+        
+        label.text = "Hello, world!"
+        label.font = UIFont.systemFont(ofSize: 17)
     }
-
+    
+    private func setupConstraints() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.centerXAnchor,
+                constant: 0),
+            label.centerYAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.centerYAnchor,
+                constant: 0),
+        ])
+    }
 
 }
 
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ViewController()
+            .preview()
+    }
+}
+#endif
